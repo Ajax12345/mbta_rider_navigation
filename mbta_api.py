15647dba90f9f82be4e1f975680373f4176a872d
Ajax12345 <jpetullo14@gmail.com>
2023-11-03 14:42:27 -0400

    updates

--- mbta_api.py
+++ mbta_api.py
@@ -12,7 +12,7 @@ def lines() -> None:
             running_data = json.load(f)
 
         with open('json_data/lines.json', 'w') as f1:
-            json.dump(running_data + d['data'], f1)
+            json.dump(running_data + d['data'], f1, indent=4)
     
         if d['links']['last'] == d['links']['next']:
             break
@@ -28,5 +28,16 @@ def check_lines() -> None:
         print(i['attributes']['long_name'], i['id'])
         print('-'*20)
 
+
+def routes() -> None:
+    with open('json_data/routes.json', 'a') as f:
+        json.dump(requests.get('https://api-v3.mbta.com/routes?filter[direction_id]=1&filter[type]=2&page[limit]=100&page[offset]=0&sort=long_name').json()['data'], f, indent = 4)
+
+
+def stops() -> None:
+    with open('json_data/stops1.json', 'a') as f:
+        json.dump(requests.get('https://api-v3.mbta.com/stops?filter[direction_id]=0&filter[route]=CR-Fitchburg&page[limit]=100&page[offset]=0&sort=name').json()['data'], f, indent = 4)
+
+
 if __name__ == '__main__':
-    check_lines()
+    stops()
