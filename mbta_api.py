7905a4ba2769cf299040e263d16aa9ffc709635a
Ajax12345 <jpetullo14@gmail.com>
2023-11-03 15:55:09 -0400

    vehicles added

--- mbta_api.py
+++ mbta_api.py
@@ -1,4 +1,5 @@
 import requests, json, time
+import datetime, re
 
 API_KEY = 'ec477916907d435d9cdc835309d1a9f0'
 
@@ -38,6 +39,26 @@ def stops() -> None:
     with open('json_data/stops1.json', 'a') as f:
         json.dump(requests.get('https://api-v3.mbta.com/stops?filter[direction_id]=0&filter[route]=CR-Fitchburg&page[limit]=100&page[offset]=0&sort=name').json()['data'], f, indent = 4)
 
+def to_dt(s:str) -> datetime.datetime:
+    return datetime.datetime(*map(int, re.findall('\d+', s)[:6]))
+
+def vehicles() -> None:
+    trains = requests.get('https://api-v3.mbta.com/vehicles?filter[direction_id]=0&filter[route]=CR-Fitchburg&filter[route_type]=2&page[limit]=100&page[offset]=0&sort=current_stop_sequence').json()['data']
+    for d in trains:
+        direction_id = d['attributes']['direction_id']
+        updated_at = d['attributes']['updated_at']
+        route, stop, trip = [d['relationships'][i]['data']['id'] for i in ['route', 'stop', 'trip']]
+        print(direction_id, route, stop, trip)
+        print(json.dumps(d, indent=4))
+        [prediction] = requests.get(f'https://api-v3.mbta.com/predictions?filter[direction_id]={direction_id}&filter[route]={route}&filter[route_type]=2&filter[stop]={stop}&filter[trip]={trip}&page[limit]=100&page[offset]=0&sort=arrival_time').json()['data']
+        print('prediction')
+        print(json.dumps(prediction, indent=4))
+        schedule = requests.get(f'https://api-v3.mbta.com/schedules?filter[direction_id]={direction_id}&filter[route]={route}&filter[route_type]=2&filter[stop]={stop}&filter[trip]={trip}&page[limit]=100&page[offset]=0&sort=arrival_time').json()['data']
+        scheduled = [i for i in schedule if to_dt(i['attributes']['arrival_time']) <= to_dt(prediction['attributes']['arrival_time'])][-1]
+        print('scheduled')
+        print(json.dumps(scheduled, indent=4))
+        print('-'*30)
+
 
 if __name__ == '__main__':
-    stops()
+    vehicles()
